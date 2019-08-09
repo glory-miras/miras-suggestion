13860832324f8e316308fe816d1e886791baafc9
iFaujack <fauzi.nurfadillahh@gmail.com>
2019-08-09 11:28:13 +0700

    advice-slip

--- server/controllers/beerController.js
+++ server/controllers/beerController.js
@@ -1,15 +1,19 @@
 const Beer = require('../models/beer')
+const AdviceSlip = require('../models/adviceSlip')
 const beerParser = require('../helpers/beerParserData')
 
 class BeerController {
     static getAll(req,res){
         if (req.query.id){
+            let beers = [];
             const { id } = req.query
             Beer.getById(id)
             .then(({data}) => {
-                let beers = [];
                 data.forEach(el => { beers.push(beerParser(el))})
-                res.json(beers)
+                return AdviceSlip.randomAdvice()
+            })
+            .then(({data}) => {
+                res.json({data, beers})
             })
             .catch(err => {
                 res.send(err)
