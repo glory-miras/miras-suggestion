fef8fe34c5409db3a0e54b46971381f1dc699cc4
iFaujack <fauzi.nurfadillahh@gmail.com>
2019-08-09 09:05:36 +0700

    init

--- server/controllers/beerController.js
+++ server/controllers/beerController.js
@@ -1,7 +1,34 @@
 const Beer = require('../models/beer')
-const beerParser = require('')
+const beerParser = require('../helpers/beerParserData')
+
 class BeerController {
     static getAll(req,res){
-        
+        Beer.getAll()
+        .then(({data}) => {
+            let beers = [];
+            data.forEach(el => { beers.push(beerParser(el))})
+            res.json(beers)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    static getById(req,res){
+        const { id } = req.params
+        // const id = 4;
+        Beer.getById(id)
+        .then(({data}) => {
+            let beers = [];
+            data.forEach(el => { beers.push(beerParser(el))})
+            res.json(beers)
+            // console.log(beers)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 }
+
+module.exports = BeerController
+// BeerController.getById(3)
