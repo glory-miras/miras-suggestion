8b4e582452cb864e6af3025da5fc3adba7092e1b
saveroz <muhammadsavero@gmail.com>
2019-11-17 19:08:32 +0700

    ngetest

--- server/app.js
+++ server/app.js
@@ -9,9 +9,7 @@ const mongoose = require('mongoose');
 const erroHandler = require('./middleware/errorHandler')
 const beerRoute = require('./routes/beerRoute')
 
-
 mongoose.connect('mongodb://localhost/mirasApp', {useNewUrlParser : true})
-
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
@@ -20,3 +18,7 @@ app.use('/beers',beerRoute)
 app.use(erroHandler)
 
 app.listen(port)
+.then(()=>{
+console.log("listening to port")})
+.catch(err=>{
+console.log(err)}
